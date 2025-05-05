4884a792cd98875d27a0730f7216e1104fdac7d1
Siam <siamekanto360@gmail.com>
2025-05-05 14:34:04 +0600

    update gradient color

--- components/TopBanner.tsx
+++ components/TopBanner.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const TopBanner = () => {
   return (
-    <div className='w-screen h-[76px] lg:h-[46px] fixed top-0 lg:static z-30 flex items-center text-center justify-center bg-gradient-to-r from-[#FC004E] to-[#10CBE0] text-white font-extrabold text-[16px] lg:text-[22px]'>
+    <div
+      style={{ background: 'linear-gradient(90deg, #FC004E 0%, #10CBE0 100%)' }}
+      className='w-screen h-[76px] lg:h-[46px] fixed top-0 lg:static z-30 flex items-center text-center justify-center text-white font-extrabold text-[16px] lg:text-[22px]'
+    >
       <div className='x-container'>
         <span className='text-[hsl(184,100%,49%)]'>
           🚀 FRESH BEGINNINGS SALE:
